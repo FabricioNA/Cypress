976889ad0af47efca482c52bcc65f95f0016dfa7
Fabricio N. <fabricioaleixo12@gmail.com>
2025-02-02 17:44:02 -0300

    git init

---
+++ e2e/#1/contact-us.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+describe("Test Contact Us form via WebdriverUni", () => {
+    it("Should be able to submit a successful submission via contact us form", () => {
+        //cypress code
+        cy.get('');
+    });
+
+    it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
+        //cypress code
+    });
+})
+
