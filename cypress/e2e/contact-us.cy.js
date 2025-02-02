07a6d31b0aab368ee004f664a9a956f2ae80ba33
Fabricio N. <fabricioaleixo12@gmail.com>
2025-02-02 19:48:00 -0300

    contact us

---
+++ cypress/e2e/contact-us.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="Cypress" />
+
+describe("Test Contact Us form via WebdriverUni", () => {
+    it("Contact Us", () => {
+        //cypress code
+        cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.get('[name="first_name"]').type("Fabricio")
+        cy.get('[name="last_name"]').type("Aleixo")
+        cy.get('[name="email"]').type("fabricio@getmail.com.br")
+        cy.get('textarea.feedback-input').type("TesteAddCyGet")
+        cy.get('[type="submit"]').click()
+    });
+})
+
