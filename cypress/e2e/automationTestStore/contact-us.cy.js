6612360c67a4bab5e261367ffd22f678cc44d3a6
Fabricio N. <fabricioaleixo12@gmail.com>
2025-02-05 23:07:08 -0300

    xpath e Assertions

--- cypress/e2e/automationTestStore/contact-us.cy.js
+++ cypress/e2e/automationTestStore/contact-us.cy.js
@@ -1,12 +1,21 @@
 /// <reference types="Cypress" />
 
 describe("Test Contact Us form via automationTestStore", () => {
-    it.only("Contact Us - Successful", () => {
+    it.only("Contact Us - xpath", () => {
+        cy.visit("https://automationteststore.com/")
+        cy.xpath("//a[contains(@href, 'contact')]").click()  
+        cy.xpath('//input[@name = "first_name"]').type("Fabricio Aleixo");
+        cy.xpath('//input[@id = "ContactUsFrm_email"]').type("fabricio@xpath.com.br");
+        cy.xpath('//textarea[@name = "enquiry"]').type("testexpath");
+        cy.get("button[title='Submit']").click()
+        cy.get('.mb40 > :nth-child(3)').should('have.text', 'Your enquiry has been successfully sent to the store owner!')
+    })
+    it("Contact Us - Successful", () => {
         cy.visit("https://automationteststore.com/")
         cy.get('.info_links_footer > :nth-child(5) > a').click()
         cy.get('#ContactUsFrm_first_name').type("Fabricio Aleixo")
         cy.get('#ContactUsFrm_email').type("fabricio@getmail.com.br")        
         cy.get('#ContactUsFrm_enquiry').type("TesteAddCyGet")
-        cy.get('.col-md-6 > .btn')
-    });
+        cy.get('.col-md-6 > .btn').click()
+    })
 });
